2fc257ed98f032accc8c1b90e67a279191ba1e5c
LaisFnanda <lais.589@gmail.com>
2024-08-19 19:56:33 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    {
+     
+        public double Numero1 { get; set;  }
+        public double Numero2 { get; set; }
+
+        //Construtor
+        public Calculadora(double _numero1, double _numero2)
+        { 
+            Numero1 = _numero1;
+            Numero2 = _numero2;
+        }
+
+        public double Somar () => Numero1 + Numero2;
+        public double Subtrair() => Numero1 - Numero2;
+        public double Multiplicar () => Numero1 * Numero2;
+
+
+    }
+}
---
+++ MostrarCalculadora/Program.cs
@@ -0,0 +1,2 @@
+﻿
+Calculadora calc = new Calculadora();
